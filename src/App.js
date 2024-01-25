1b81bcf06d63dd2f8fa06287a9622508adeb0421
seemadhamiwal <seema.dhamiwal@jarvis.consulting>
2024-01-25 13:00:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    const arr = [2,3,1,5,4,7];
+    const arr1 = arr.sort(function (a,b){return a - b })
+
+    document.getElementById("demo").innerHTML = arr1;
+  return (   
+        <p id="demo"> result</p>
+        
   );
 }
 
